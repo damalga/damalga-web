b23ec5a5495d0ce16560b17fe10cc7a3fc8e9c57
damalga <dam.al.ga@gmx.com>
2025-11-12 18:20:28 +0100

    obs up date

--- src/Column2.jsx
+++ src/Column2.jsx
@@ -1,6 +1,7 @@
+import nllp from "./assets/svgs/nllp.svg";
 import alcarcia from "./assets/svgs/alcarcia.svg";
 import polymorphism from "./assets/svgs/polymorphism.svg";
-import nllp from "./assets/svgs/nllp.svg";
+import hackeed from "./assets/svgs/hackeed.svg";
 import css from "./assets/svgs/css.svg";
 import git from "./assets/svgs/git.svg";
 import nodee from "./assets/svgs/nodee.svg";
@@ -31,13 +32,11 @@ function Column2() {
           </p>
         </div>
         <p className="first extra-info">
-          Both projects take electronic music as their starting point. I collect
-          vinyls and I'm fascinated by synths, drum machines and basically any
-          gadget or device that makes noise.
+          Both projects take electronic music as their starting point. I collect vinyls and I'm fascinated by synths, drum machines and basically any gadget or device that makes noise.
         </p>
-        <div className="polymorphism music project">
+        <div className="nllp music project">
           <img src={nllp} alt="nllp logo" />
-          <p className="bold">Damalga NL + LP<br></br>
+          <p className="bold">NL + LP<br></br>
             <span>NewsLetter + Landing Page</span>
             <br></br>
             <a className="link" href="https://damalga.github.io/damalga-nl-lp/" target="_blank">Website</a>
@@ -46,46 +45,46 @@ function Column2() {
         <p className="first extra-info">
           This is a project I started recently to consolidate ideas about technology, economy, philosophy and beyond!
         </p>
+        <div className="nllp music project">
+          <img src={hackeed} alt="nllp logo" />
+          <p className="bold">Hackeed<br></br>
+            <span>E-commerce</span>
+            <br></br>
+            <a className="link" href="https://hackeed.es" target="_blank">Website</a>
+          </p>
+        </div>
+        <p className="first extra-info">
+          Open tech store and living lab driven by curiosity, where ethics flex and systems rebuild.
+        </p>
       </div>
 
       <div className="skills topic">
         <h4>SKILLS</h4>
         <div className="on-off">
-          <h5>Print & Digital</h5>
+          <h5>Design and Composition</h5>
           <p>
-            With a strong mix of references and resources, I bring brand values to life across digital and print media. Check out my <a className="link" href="https://www.behance.net/damalga" target="_blank">Behance</a> for some of my favorite projects from my creative journey.
+            With a strong mix of references and resources, I bring brand values to life across digital and print media. Check out my <a className="link" href="https://www.behance.net/damalga" target="_blank">Behance</a> for some of my favorite projects from my visual journey.
           </p>
         </div>
         <div className="front-end">
-          <h5>Front-end web tools</h5>
+          <h5>Systems and Interfaces</h5>
           <div className="front-end-icons">
             <div className="css">
               <img src={css} alt="bootstrap" />
-              <p className="text">High level in HTML and CSS, with solid knowledge of <a className="link" href="https://handlebarsjs.com/" target="_blank">Handlebars</a> for dynamic templating, as well as <a className="link" href="https://sass-lang.com/" target="_blank">Sass</a> and various stylesheet libraries. Skilled at efficiently setting and styling elements on the screen.
-              </p>
+              <p className="text"> I build interfaces from the ground up, keeping structure and style in sync. HTML and CSS are my foundation, extended with <a className="link" href="https://sass-lang.com/" target="_blank">Sass</a> for modular styling. For static sites, <a className="link" href="https://handlebarsjs.com/" target="_blank">Handlebars</a> is my go-to, simple, fast and easy to maintain.</p>
             </div>
             <div className="node">
               <img src={nodee} alt="node" />
-              <p className="text">Mid-level in JavaScript. I keep up with modern tools and frameworks, but always come back to the elegance of web standards, vanilla Js and try to keep things dependency-free.</p>
+              <p className="text">When it comes to interaction and logic I stay close to vanilla JavaScript. I’m comfortable enough with <a className="link" href="https://react.dev/" target="_blank">React</a>, but <a className="link" href="https://vuejs.org/" target="_blank">Vue</a> fits better with how I build and think. It lets me handle both templating and state with clarity.</p>
             </div>
             <div className="git">
               <img src={git} alt="git" />
-              <p className="text">
-                <del>Low level</del> Beginner in...<br></br>
-                everything, except giving up.<br></br>
-                Full coding energy.<br></br>
-                <a className="link" href="https://github.com/damalga" target="_blank">Github</a> and <a className="link" href="https://bitbucket.org/damalga/" target="_blank">Bitbucket</a>. Ready to fuse design and tech with creativity and code.
+              <p className="text">I work comfortably in the terminal, moving between <a className="link" href="https://git-scm.com/" target="_blank">Git</a> and <a className="link" href="https://github.com/damalga" target="_blank">Github</a> to keep projects alive and traceable. Bash is my daily companion for small automations, quick fixes and clean setups.
               </p>
             </div>
           </div>
           <p className="extra-info">
-            Countless front-end experiences in projects built on React, Ruby on Rails, Symfony... and CMSs like Liferay, Drupal, WordPress, and more... So, adapting to build any view or interaction with any core comes naturally to me.
-            <br></br>
-            <br></br>
-            I used to work with Gulp to optimize my code, but since <a className="link" href="https://vite.dev/" target="_blank">Vite</a> showed up, it's been my main tool for bundling. It's cool how fast it runs in development.
-            <br></br>
-            <br></br>
-            Oh! and I’m pretty comfortable with the terminal,Git and Linux. I (don't) use Arch btw XD... Currently using Debian on one machine and Manjaro on another.
+            I’ve worked across many cores and CMSs, adapting easily to different setups and workflows. I used to orchestrate builds with <a className="link" href="https://gulpjs.com/" target="_blank">Gulp</a>, now <a className="link" href="https://vitejs.dev/" target="_blank">Vite</a> does the job faster and cleaner. I work comfortably in Linux, where most of my tools and setups live.
           </p>
         </div>
       </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -11,13 +11,13 @@ function Header() {
           </h1>
         </div>
         <div className='intro'>
-          <h2>WHO I AM</h2>
-          <p>I'm Damalga (that's an acronym of my full name). Hailing from Madrid. I have a deep affinity for crafting simple and functional designs, with a penchant for removing the unnecessary to produce smart code and clean aesthetics. My commitment to honing my design and development skills is well ingrained and I thrive on embracing new experiences.</p>
+          <h2>WHO AM I</h2>
+          <p>I'm Damalga (that’s an acronym of my full name), hailing from Madrid. I have a deep affinity for crafting simple and functional designs, with a penchant for removing the unnecessary to assemble smart code and clean aesthetics. My commitment to honing my design and development skills is well ingrained. I like to build solid systems and I thrive on embracing new experiences.</p>
         </div>
       </section>
       <section className='second'>
         <div className='name'>
-          <h3>GRAPHIC DESIGNER AND CREATIVE WEB DEVELOPER</h3>
+          <h3>SYSTEMS BUILDER AND CREATIVE WEB CODER</h3>
         </div>
       </section>
     </header>
